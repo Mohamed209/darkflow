7d7bb55da45c0844ba0dd8513f2e03e3d43f35a7
mohamed mosad <mohamedmosad209@gmail.com>
2020-02-06 17:37:56 +0200

    fixed mmpeg

--- video_configs.sh
+++ video_configs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Download test video"
 wget --header="Host: doc-0s-9s-docs.googleusercontent.com" --header="User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36" --header="Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" --header="Accept-Language: en-US,en;q=0.9,ar-EG;q=0.8,ar;q=0.7" --header="Referer: https://drive.google.com/drive/u/0/my-drive" --header="Cookie: AUTH_u2j0t6bv6dftjmt2lsba7r3rvpi7gdis_nonce=svp6fobcsj6d2" --header="Connection: keep-alive" "https://doc-0s-9s-docs.googleusercontent.com/docs/securesc/3dqr5f7kd72k46kubklelqd888nobt2a/g8b39h4f5jugi6g87ohu5ln3kmopk0t1/1581002100000/11516553764628263985/01449274755406512808/1k_F39n6sHWVD3gplhVZLmY73BW2qtzrq?e=download&authuser=0&nonce=svp6fobcsj6d2&user=01449274755406512808&hash=mal6cc0clt5gb5bkcr2rp2q4hiot8lhs" -O "GP010080.mov" -c
+apt-get install --reinstall ca-certificates
 apt-add-repository ppa:jonathonf/ffmpeg-3
 apt-get update
 apt-get install ffmpeg # to cut sample videos
